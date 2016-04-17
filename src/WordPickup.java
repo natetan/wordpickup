f85e923b45ee26a1feb2eea9e9e9c0e21a30cd55
yulongtan <swifties@uw.edu>
2016-04-17 13:41:09 -0700

    added map to keep track of words

--- src/WordPickup.java
+++ src/WordPickup.java
@@ -1,6 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 /**
- * Created by Yulong on 4/16/2016.
+ * Yulong Tan
+ * 4.16.16
+ * <p>
+ * This class keeps track of the various cheeky sayings from
+ * word pickups
  */
 
 public class WordPickup {
+
+    public static void main(String[] params) throws FileNotFoundException {
+        File file = new File("pickups.txt");
+        Scanner input = new Scanner(file);
+        Map<String, String> words = new TreeMap<>();
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            String[] parts = line.split("[|]");
+            if (!words.containsKey(parts[0])) {
+                words.put(parts[0].trim(), parts[1].trim());
+            }
+        }
+        System.out.println("Current words: " + words.keySet());
+    }
 }
